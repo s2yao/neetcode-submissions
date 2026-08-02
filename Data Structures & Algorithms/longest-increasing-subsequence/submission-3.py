e5f1445a4c7167d3b1cc4b98cd5375a0ddc28a4e
LintFail <132319682+s2yao@users.noreply.github.com>
2026-08-02 16:59:27 -0400

    Add: longest-increasing-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-3.py
@@ -0,0 +1,21 @@
+from bisect import bisect_left
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = []
+        # record current 
+        dp.append(nums[0])
+
+        for i in range(1, len(nums)):
+            if nums[i] > dp[-1]:
+                dp.append(nums[i])
+                continue
+            
+            # find the index to replace
+            # return idx containing ele bigger than current ele
+            idx = bisect_left(dp, nums[i])
+            dp[idx] = nums[i]
+
+        return len(dp)
+
+        # 替换不加长度
+        # 只有append加，append only when curr ele > last element -- def of LIS
