0355ddaa567a9bb6d29459dcb5eb46b986f7678a
LintFail <132319682+s2yao@users.noreply.github.com>
2026-08-03 18:27:37 -0400

    Add: valid-parenthesis-string - submission-4

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        stack_idx = []
+        asterisk_idx = [False] * len(s) 
+
+        def find_asterisk(start, end):
+            for idx in reversed(range(start, end)):
+                if asterisk_idx[idx] == True:
+                    asterisk_idx[idx] = False
+                    return True
+            return False
+                
+        for idx in range(len(s)):
+            if s[idx] == "*":
+                asterisk_idx[idx] = True
+            elif s[idx] == ")":
+                if not stack_idx:
+                    if not find_asterisk(0, idx):
+                        return False
+                elif s[stack_idx[-1]] != "(":
+                    if not find_asterisk(stack_idx[-1], idx):
+                        return False
+                # there is corresponding
+                else:
+                    stack_idx.pop()
+            else:
+                stack_idx.append(idx)
+        
+        while stack_idx:
+            idx = stack_idx.pop()
+
+            if not find_asterisk(idx, len(s)):
+                return False
+        
+        return True
