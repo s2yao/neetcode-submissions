6344279dc3a6be1df071d0e04fc24e042ec04458
LintFail <132319682+s2yao@users.noreply.github.com>
2026-09-08 00:45:23 -0400

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.py
@@ -0,0 +1,21 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key = lambda x: x.end)
+
+
+        for i in range(len(intervals) - 1):
+            if intervals[i].end <= intervals[i+1].start:
+               continue
+            else:
+                return False
+        
+        return True
+            
